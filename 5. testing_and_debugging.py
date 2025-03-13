25e885f8e35ad28a3f56aed9eb1243acc726d0f8
ebenofere <eofere@gmail.com>
2025-03-13 12:24:25 -0400

    update

--- 5. testing_and_debugging.py
+++ 5. testing_and_debugging.py
@@ -5,3 +5,11 @@
 # There are two reasons for this change:
 # 1. It's more realistic. In the real world, you'll be writing unit tests and running them against your code to make sure it works as expected.
 # 2. You can run and debug your code with print statements, and leave those print statements in when you submit. Unlike the output-based lessons, you won't have to remove your print statements to pass.
+
+# The pass keyword is a way to tell Python to do nothing. You'll need to replace it with your own code.
+
+
+def total_xp(level, xp_to_add):
+    level_weight = 100
+    total_xp = level * level_weight + xp_to_add
+    return total_xp
